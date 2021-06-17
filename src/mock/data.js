bb67432db7b830208ab44defde6f4ee5d90b0b5e
vvkong <wangrenxing87@gmail.com>
2021-06-17 09:09:59 +0800

    axios封装,mock数据配置

---
+++ src/mock/data.js
@@ -0,0 +1,23 @@
+// 首页
+const index = {
+    "code": 0,
+    "msg": "",
+    "swiper": [
+        {
+            "link": "https://www.baidu.com",
+            "image": "http://youimg1.c-ctrip.com/target/tg/004/531/381/4339f96900344574a0c8ca272a7b8f27.jpg"
+        },
+        {
+            "link": "https://qq.com",
+            "image": "http://img.juimg.com/tuku/yulantu/140703/330746-140F301555752.jpg"
+        },
+        {
+            "link": "https://jd.com",
+            "image": "http://pic1.nipic.com/2008-11-20/20081120122029174_2.jpg"
+        }
+    ]
+};
+
+module.exports = {
+    index
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
-module.exports = {
+const express = require('express');
+const app = express();
+var appData = require('./src/mock/data.js');
+var router = express.Router();
+app.use('/api', router);
 
+module.exports = {
+    devServer: {
+        before(app) {
+            app.get('/api/index', (req, res) => {
+                res.json({
+                    code: 0,
+                    msg: 'success',
+                    data: appData.index
+                })
+            })
+        }
+    }
 }
